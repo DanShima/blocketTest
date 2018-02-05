15ab1e66c9e539e316f668f3f3ab4c746eba9406
DanShima <giddy_with_panda@hotmail.com>
2018-02-05 23:40:38 +0100

    added test method for log in

--- src/automationTest/FirstTestCase.java
+++ src/automationTest/FirstTestCase.java
@@ -1,4 +1,6 @@
 package automationTest;
+import java.util.concurrent.TimeUnit;
+
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -14,20 +16,22 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class FirstTestCase {
 	private static WebDriver driver = null;	
 	private static WebElement element;
-	
-	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.gecko.driver","C:\\SeleniumGecko\\geckodriver.exe");
-		
-		logInTest();		
-        // Close the driver
-        driver.quit();
-	}
 	
 	
-	public static void logInTest() {
-		//open Firefox browser
-        driver = new FirefoxDriver();
+	 @BeforeClass
+     public static void openBrowser(){
+		 System.setProperty("webdriver.gecko.driver","C:\\SeleniumGecko\\geckodriver.exe");	
+         driver = new FirefoxDriver();
+         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		} 
+	 
+	/**
+	 * testing log in with user credentials
+	 */
+	@Test
+	public void logInTest() {
+		System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());		
         //launch Blocket website
         driver.get("https://www.blocket.se/");
         //go to the login page
@@ -38,16 +42,46 @@ public class FirstTestCase {
         
 		driver.findElement(By.id("CredIdentifier")).sendKeys("giddy_with_panda@hotmail.com");
 		driver.findElement(By.id("CredPassword")).sendKeys("woaini88!");
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//button[@type='submit']")).click();		
+		
 		try{
-			element = driver.findElement (By.xpath(".//*[@href='https://www.blocket.se/spid/logout']/a"));
+			 element = driver.findElement (By.cssSelector("a[href='https://www.blocket.se/spid/logout']"));
+			 element.click();
+			 System.out.println("found log out");
 		 }catch (Exception e){
 			}
 	     Assert.assertNotNull(element);
 	     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
     }
+	
+	/**
+	 * testing log in with fake, unregistered email and password
+	 */	
+	 @Test
+     public void invalidLogInTest() {
+     
+		 System.out.println("Starting test " + new Object(){}.getClass().getEnclosingMethod().getName());
+	     driver.get("https://www.blocket.se/konto/senaste_sidan");	
+	     driver.findElement(By.id("CredIdentifier")).sendKeys("fakeemail@hotmail.com");
+	     driver.findElement(By.id("CredPassword")).sendKeys("fake123");
+	     driver.findElement(By.xpath("//button[@type='submit']")).click();	
+	     try{
+			element = driver.findElement (By.cssSelector("a[href='https://www.blocket.se/spid/logout']"));
+	     }catch (Exception e){
+			}
+	     Assert.assertNull(element);
+	     System.out.println("Ending test " + new Object(){}.getClass().getEnclosingMethod().getName());
+     }
+	
+	
+	 @AfterClass
+	 public static void closeBrowser(){
+		 driver.quit();
+	 }
+	
+	
 		
 	}
 		
 
-}
+
